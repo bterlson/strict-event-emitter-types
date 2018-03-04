f10c15394b19f4e9f299929720ef4807ac1bbcc0
Brian Terlson <brian.terlson@microsoft.com>
2018-03-04 12:21:18 -0800

    various cleanups

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,48 @@ export type MatchingKeys<T, U, K extends keyof T = keyof T> =
 
 export type VoidKeys<Record> = MatchingKeys<Record, void>;
 
-type TypedEventEmitter<EmitterType, Events, EmitEvents = Events> = Pick<EmitterType, Exclude<keyof EmitterType, 'on' | 'emit' | 'addListener' | 'once' | 'removeListener'>> &
+export interface InstantiationRecord<T, U, V> {
+  _emitterType?: T,
+  _eventsType?: U,
+  _emitType?: V
+}
+
+export type TypedEventEmitter<
+  TEmitterType,
+  TEventRecord,
+  TEmitRecord = TEventRecord,
+  EventVK extends VoidKeys<TEventRecord> = VoidKeys<TEventRecord>,
+  EventNVK extends Exclude<keyof TEventRecord, EventVK> =  Exclude<keyof TEventRecord, EventVK>,
+  EmitVK extends VoidKeys<TEmitRecord> = VoidKeys<TEmitRecord>,
+  EmitNVK extends Exclude<keyof TEmitRecord, EmitVK> =  Exclude<keyof TEmitRecord, EmitVK>
+  > =
+  InstantiationRecord<TEmitterType, TEventRecord, TEmitRecord> &
+  Pick<TEmitterType, Exclude<keyof TEmitterType, 'on' | 'emit' | 'addListener' | 'once' | 'removeListener'>> &
   {
-    on<P extends Exclude<keyof Events, VoidKeys<Events>>>(event: P, listener: (m: Events[P], ...args: any[]) => void): any
-    on<P extends VoidKeys<Events>>(event: P, listener: () => void): any;
+    on<P extends EventNVK>(event: P, listener: (m: TEventRecord[P], ...args: any[]) => void): any;
+    on<P extends EventVK>(event: P, listener: () => void): any;
 
-    addListener<P extends Exclude<keyof Events, VoidKeys<Events>>>(event: P, listener: (m: Events[P], ...args: any[]) => void): any
-    addListener<P extends VoidKeys<Events>>(event: P, listener: () => void): any;
+    addListener<P extends EventNVK>(event: P, listener: (m: TEventRecord[P], ...args: any[]) => void): any
+    addListener<P extends EventVK>(event: P, listener: () => void): any;
 
-    removeListener<P extends VoidKeys<Events>>(event: P, listener: Function): any;
+    removeListener<P extends EventVK>(event: P, listener: Function): any;
 
-    once<P extends Exclude<keyof Events, VoidKeys<Events>>>(event: P, listener: (m: Events[P], ...args: any[]) => void): any
-    once<P extends VoidKeys<Events>>(event: P, listener: () => void): any;
+    once<P extends EventNVK>(event: P, listener: (m: TEventRecord[P], ...args: any[]) => void): any
+    once<P extends EventVK>(event: P, listener: () => void): any;
 
-    emit<P extends Exclude<keyof EmitEvents, VoidKeys<EmitEvents>>>(event: P, request: EmitEvents[P]): any;
-    emit<P extends VoidKeys<EmitEvents>>(event: P): any;
+    emit<P extends EmitNVK>(event: P, request: TEmitRecord[P]): any;
+    emit<P extends EmitVK>(event: P): any;
   }
 
 export default TypedEventEmitter;
 
-export function wrap<Events extends object, EmitEvents extends object = Events, T={}>(t: T): TypedEventEmitter<T, Events, EmitEvents> & T {
-  return t as any; // unsure why this is necessary
-}
+export type NoUndefined<T> = T extends undefined ? never : T;
+export type Broadcast<
+  TEmitter extends InstantiationRecord<any, any, any>,
+  TEmitRecord extends NoUndefined<TEmitter['_emitType']> = NoUndefined<TEmitter['_emitType']>,
+  VK extends VoidKeys<TEmitRecord> = VoidKeys<TEmitRecord>,
+  NVK extends Exclude<keyof TEmitRecord, VK> =  Exclude<keyof TEmitRecord, VK>
+  > = {
+    <E extends NVK>(event: E, request: TEmitRecord[E]): void;
+    <E extends VK>(event: E): void;
+  }
