46bbe4f1e9e476a5e02520fbc3265917ba6b61b8
Brian Terlson <brian.terlson@microsoft.com>
2018-03-06 09:22:44 -0800

    Add tests

---
+++ test/test.ts
@@ -0,0 +1,60 @@
+// note that this file is never run, it's only type assertions
+// currently unsure how to do negative tests.
+
+import StrictEventEmitter, { StrictBroadcast } from '../src/index';
+
+
+// set up some events
+interface Position { x: number, y: number };
+interface Events {
+  move: Position,
+  done: void
+}
+
+interface Emitables {
+  moveRequest: Position,
+  stop: void
+}
+// test with Node's event emitter
+import { EventEmitter, } from 'events';
+
+let ee = new EventEmitter;
+
+let eei: StrictEventEmitter<EventEmitter, Events> = new EventEmitter();
+function cb(p: Position) { }
+eei.on('move', v => v.x + v.y);
+eei.on('done', function () { });
+eei.emit('move', { x: 1, y: 2 });
+eei.emit('done');
+eei.addListener('move', cb);
+eei.addListener('done', function () { });
+eei.once('move', v => v.x + v.y);
+eei.once('done', function () { });
+eei.removeListener('move', cb);
+eei.removeListener('done', function () { });
+eei.emit('move', { x: 1, y: 2 });
+eei.emit('done');
+
+let eei2: StrictEventEmitter<EventEmitter, Events, Emitables> = new EventEmitter();
+eei2.on('move', v => v.x + v.y);
+eei2.on('done', function () { });
+eei2.addListener('move', v => v.x + v.y);
+eei2.addListener('done', function () { });
+eei2.once('move', v => v.x + v.y);
+eei2.once('done', function () { });
+eei2.removeListener('move', v => v.x + v.y);
+eei2.removeListener('done', function () { });
+eei2.emit('moveRequest', { x: 1, y: 2 });
+eei2.emit('stop');
+
+var broadcast: StrictBroadcast<typeof eei2> = function (e: string, payload?: any) {
+  if (e === 'move') {
+    eei.emit(e, payload);
+  } else if (e === 'done') {
+    eei.emit(e);
+  }
+
+  throw 'unknown event';
+};
+broadcast('stop');
+broadcast('moveRequest', { x: 1, y: 2 });
